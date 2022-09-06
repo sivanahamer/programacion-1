f8416858b76069b7e31a6db95a22777c3091a3b2
Sivana Alexa Hamer <sivanahamer@gmail.com>
2022-09-06 16:09:52 -0600

    Added basic pizza class

---
+++ 04-OOP/src/pizza/Pizza.java
@@ -0,0 +1,22 @@
+package pizza;
+
+class Pizza{
+    private double precio;
+    private Topping [] toppings;
+    private Size size;
+    private TipoMasa masa;
+    private EstadoPizza estado;
+
+    public void cocinar(){
+
+    }
+
+    //TODO
+    public void ver(){
+
+    }
+
+    public void armar(){
+
+    }
+}
