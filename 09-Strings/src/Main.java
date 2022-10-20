27e757fa628c06ed99f98db05906242ec7d76e29
Sivana Alexa Hamer <sivanahamer@gmail.com>
2022-10-20 15:11:00 -0600

    Added basic strings

---
+++ 09-Strings/src/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String [] args){
+        System.out.println("Alfabeto ruso");
+        for(int i = 65; i <= 122; i++){
+                System.out.print((char) i);
+            }
+            System.out.println();
+        }
+}
